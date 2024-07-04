f6777f2c8717a94a47db2d0234761fae063ed909
Paulo Victor Lima <pvldotz@gmail.com>
2024-07-04 18:44:24 -0300

    mudanas home

--- script.js
+++ script.js
@@ -1,11 +1,74 @@
 const menu = document.querySelector('#navbar-icon');
-const navlist = document.querySelector('.nav-list');
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('active');
 }
 
 const header = document.querySelector("header");
-window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky", window.scrollY > 0);
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var sections = document.querySelectorAll('section');
+
+    sections.forEach(function(section) {
+        var top = section.offsetTop - document.querySelector('header').offsetHeight;
+        var bottom = top + section.clientHeight;
+
+        if (scrollPosition >= top && scrollPosition < bottom) {
+            var sectionId = section.id;
+
+            var activeLink = document.querySelector('.nav-link[href="#' + sectionId + '"]');
+            if (activeLink) {
+                document.querySelectorAll('.nav-link').forEach(function(navLink) {
+                    navLink.classList.remove('active');
+                });
+                activeLink.classList.add('active');
+            }
+
+            var activeTitle = document.querySelector('.title.active-title');
+            if (activeTitle) {
+                activeTitle.classList.remove('active-title');
+            }
+
+            var titleInCurrentSection = section.querySelector('.title');
+            if (titleInCurrentSection) {
+                titleInCurrentSection.classList.add('active-title');
+            }
+        }
+    });
 });
+
+window.addEventListener('resize', function() {
+    setActiveTab();
+});
+
+/* function setActiveTab() {
+    var scrollPosition = window.scrollY;
+    var sections = document.querySelectorAll('section');
+
+    sections.forEach(function(section) {
+        var top = section.offsetTop - document.querySelector('header').offsetHeight;
+        var bottom = top + section.clientHeight;
+
+        if (scrollPosition >= top && scrollPosition < bottom) {
+            var sectionId = section.id;
+
+            var activeLink = document.querySelector('.nav-link[href="#' + sectionId + '"]');
+            if (activeLink) {
+                document.querySelectorAll('.nav-link').forEach(function(navLink) {
+                    navLink.classList.remove('active');
+                });
+                activeLink.classList.add('active');
+            }
+        }
+    });
+} */
+
+const sr = ScrollReveal({
+    distance: '100px',
+    duration: 2000,
+    reset: true
+});
+
+//const homeText = document.querySelector('.texto')
+sr.reveal('.text', {origin:'left'});
+sr.reveal('.imgdela', {origin:'right'});
